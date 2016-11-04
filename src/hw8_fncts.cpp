2402e12583dd8304937d6f17ca2d0f3fab8d9b8e
Christopher O'Toole <cdocq9@mst.edu>
2016-11-03 19:48:07 -0500

    Added src directory and source files to project
    Better file organization and new source files with comment headers.

---
+++ src/hw8_fncts.cpp
@@ -0,0 +1,6 @@
+/*
+   Programmers: Christopher O'Toole and Yunchao Zhang
+   Date: 
+   File: hw8_fncts.cpp
+   Purpose:
+*/
---
+++ src/hw8_main.cpp
@@ -0,0 +1,6 @@
+/*
+   Programmers: Christopher O'Toole and Yunchao Zhang
+   Date: 
+   File: hw8_main.cpp
+   Purpose:
+*/
